ca1f8aee345e2163b2df60ceb96260ad714992f7
takakuni.shinnosuke <takakuni.shinnosuke@classmethod.jp>
2024-12-13 17:14:05 +0900

    [mod] 不要なポリシーの削除

--- sagemaker_hyperpod_101/iam.tf
+++ sagemaker_hyperpod_101/iam.tf
@@ -14,37 +14,6 @@ resource "aws_iam_role" "hyperpod" {
   })
 }
 
-resource "aws_iam_policy" "hyperpod_vpc" {
-  name = "sagemaker-hyperpod-vpc-policy"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "ec2:CreateNetworkInterface",
-          "ec2:CreateNetworkInterfacePermission",
-          "ec2:DeleteNetworkInterface",
-          "ec2:DeleteNetworkInterfacePermission",
-          "ec2:DescribeNetworkInterfaces",
-          "ec2:DescribeVpcs",
-          "ec2:DescribeDhcpOptions",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DetachNetworkInterface",
-          "ec2:CreateTags"
-        ]
-        "Resource" : "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "hyperpod_vpc" {
-  role       = aws_iam_role.hyperpod.name
-  policy_arn = aws_iam_policy.hyperpod_vpc.arn
-}
-
 resource "aws_iam_role_policy_attachment" "hyperpod_managed" {
   role       = aws_iam_role.hyperpod.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerClusterInstanceRolePolicy"
